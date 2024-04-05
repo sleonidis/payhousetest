08f51848cd6011c3af2707975373f7619b000a0e
siemens <2001sema@gmail.com>
2024-04-05 21:11:58 +0700

    second commit

--- resources/views/layouts/wrapper.blade.php
+++ resources/views/layouts/wrapper.blade.php
@@ -6,7 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    @vite(['resources/css/app.min.css', 'resources/js/app.js'])
+    @vite([
+ 'resources/css/app.css',
+ 'resources/js/app.js'
+ ])
 </head>
 <body class="bg-light">
 <div class="container pt-5">
